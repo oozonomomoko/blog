89e36d63f91c18d194be81396ad06b4a47ef0ec4
oozonomomoko <yin.da@qq.com>
2018-10-31 11:45:03 +0800

    update 2018-10-31

--- day/day.sh
+++ day/day.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-first="2011-11-10"
-last="2018-10-27"
+first="2018-10-27"
+last="2018-10-31"
 until [ "$first" = "$last" ]
 do
     first=`date -d "$first 1 days" +"%Y-%m-%d"`
-    wget -q http://ikuta.club/nogizaka/message/getBlogList.do?day=$first -O $first
+    wget -nc -q http://ikuta.club/nogizaka/message/getBlogList.do?day=$first -O $first
     echo $first
 done
