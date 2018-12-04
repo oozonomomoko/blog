d8288cc7b422b5ebd16ad27be51b4a1d6da2a661
oozonomomoko <yin.da@qq.com>
2018-12-05 00:14:04 +0800

    waka

--- day/day.sh
+++ day/day.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
-first="2018-11-20"
-last="2018-12-01"
+first="2018-12-04"
+last="2018-12-05"
 until [ "$first" = "$last" ]
 do
     wget -nc -q http://ikuta.club/nogizaka/message/getBlogList.do?day=$first -O $first
