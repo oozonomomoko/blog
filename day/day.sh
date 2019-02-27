06e3838a4acd822c420a5f487dc1ec1cff1846c4
oozonomomoko <yin.da@qq.com>
2019-02-27 02:31:28 -0500

    day

--- day/day.sh
+++ day/day.sh
@@ -4,7 +4,7 @@ first="2019-01-01"
 last="2019-02-26"
 until [ "$first" = "$last" ]
 do
-    wget -nc -q http://ikuta.club/nogizaka/message/getBlogList.do?day=$first -O $first
+    wget -q http://localhost:8080/nogizaka/message/getBlogList.do?day=$first -O $first
     echo $first
     first=`date -d "$first 1 days" +"%Y-%m-%d"`
 done
